224c2ada8a71b217a8ce48e1f7b9f3399f64c7ef
zhoujian <thread_zhou@126.com>
2019-11-18 17:48:25 +0800

    添加配置类，添加application.yml、application-dev.yml文件，

---
+++ light-security-client/src/main/java/com/light/security/LightSecurityClientApplication.java
@@ -0,0 +1,12 @@
+package com.light.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightSecurityClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightSecurityClientApplication.class);
+    }
+}
---
+++ light-security-core/src/main/java/com/light/security/properties/AuthenticationFilterProperties.java
@@ -0,0 +1,10 @@
+package com.light.security.properties;
+
+/**
+ * @ClassName AuthenticationFilterProperties
+ * @Description 认证过滤器配置类
+ * @Author ZhouJian
+ * @Date 2019-11-18
+ */
+public class AuthenticationFilterProperties extends AbstractFilterProperties {
+}
