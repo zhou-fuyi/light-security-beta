ecc2090182e240a7667c93d60251ce66db0acfa2
zhoujian <thread_zhou@126.com>
2019-12-04 15:46:57 +0800

    基本完成FilterChainProxy的编写

--- light-security-core/src/main/java/com/light/security/core/config/core/builder/ChainProxyBuilder.java
+++ light-security-core/src/main/java/com/light/security/core/config/core/builder/ChainProxyBuilder.java
@@ -4,13 +4,20 @@ import com.light.security.core.config.core.AbstractConfiguredSecurityBuilder;
 import com.light.security.core.config.core.ObjectPostProcessor;
 import com.light.security.core.config.core.SecurityBuilder;
 import com.light.security.core.filter.FilterSecurityInterceptor;
+import com.light.security.core.filter.chain.DefaultSecurityFilterChain;
 import com.light.security.core.filter.chain.SecurityFilterChain;
+import com.light.security.core.util.matcher.AbstractRequestMatcherRegistry;
+import com.light.security.core.util.matcher.MvcRequestMatcher;
+import com.light.security.core.util.matcher.RequestMatcher;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.Assert;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
 import javax.servlet.Filter;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,9 +53,25 @@ import java.util.List;
 public final class ChainProxyBuilder extends AbstractConfiguredSecurityBuilder<Filter, ChainProxyBuilder> implements SecurityBuilder<Filter>, ApplicationContextAware {
 
     /**
+     * 用于保存{@link #ignoredRequestRegistry}注册的{@link RequestMatcher}
+     */
+    private List<RequestMatcher> ignoredRequests = new ArrayList<>();
+
+    /**
      * 存储用于构建{@link SecurityFilterChain}的构建器对象
      */
     private final List<SecurityBuilder<? extends SecurityFilterChain>> securityFilterChainBuilders = new ArrayList<>();
+
+    /**
+     * 该对象包继承了{@link AbstractRequestMatcherRegistry 这是一个用于注册{@link RequestMatcher}的基类},
+     * 包装了几个{@link RequestMatcher}的注册方法, 该实例仅仅用于注册{@link RequestMatcher}
+     * 专门包装一个类来干这个事好像有点多余, 但是还是有必要的, 这个注册类是专门用于注册过滤器链需要忽略的请求<code> HttpServletRequest </code>
+     *
+     * 重点来了, 都说了这个类仅仅用于注册, 那么注册的需要忽略的{@link RequestMatcher}都是怎么保存的
+     * 哈, 当然是使用我们上面定义的{@link #ignoredRequests}进行注册的{@link RequestMatcher}的保存。
+     *
+     */
+    private IgnoredRequestRegistry ignoredRequestRegistry;
 
     /**
      * 授权过滤器
@@ -71,27 +94,56 @@ public final class ChainProxyBuilder extends AbstractConfiguredSecurityBuilder<F
     }
 
     /**
-     * 设置{@link FilterSecurityInterceptor}。 通常由{@link com.light.security.core.config.core.configurer.WebSecurityConfigurerAdapter}调用。
-     * @param securityInterceptor
-     * @return
+     * <p>
+     * Allows adding {@link RequestMatcher} instances that Spring Security
+     * should ignore. Web Security provided by Spring Security (including the
+     * {@link com.light.security.core.authentication.context.SecurityContext}) will not be available on {@link HttpServletRequest} that
+     * match. Typically the requests that are registered should be that of only static
+     * resources. For requests that are dynamic, consider mapping the request to allow all
+     * users instead.
+     * </p>
+     *
+     * Example Usage:
+     *
+     * <pre>
+     * webSecurityBuilder.ignoring()
+     * // ignore all URLs that start with /resources/ or /static/
+     * 		.antMatchers(&quot;/resources/**&quot;, &quot;/static/**&quot;);
+     * </pre>
+     *
+     * Alternatively this will accomplish the same result:
+     *
+     * <pre>
+     * webSecurityBuilder.ignoring()
+     * // ignore all URLs that start with /resources/ or /static/
+     * 		.antMatchers(&quot;/resources/**&quot;).antMatchers(&quot;/static/**&quot;);
+     * </pre>
+     *
+     * Multiple invocations of ignoring() are also additive, so the following is also
+     * equivalent to the previous two examples:
+     *
+     * <pre>
+     * webSecurityBuilder.ignoring()
+     * // ignore all URLs that start with /resources/
+     * 		.antMatchers(&quot;/resources/**&quot;);
+     * webSecurityBuilder.ignoring()
+     * // ignore all URLs that start with /static/
+     * 		.antMatchers(&quot;/static/**&quot;);
+     * // now both URLs that start with /resources/ and /static/ will be ignored
+     * </pre>
+     *
+     *
+     * 允许添加{@link RequestMatcher}实例，Spring Security应该忽略这些实例。
+     * Spring Security提供的Web Security（包括{@link com.light.security.core.authentication.context.SecurityContext}）
+     * 将无法在匹配的{@link HttpServletRequest}上使用。 通常，注册的请求应该仅是静态资源的请求。 对于动态请求，请考虑映射请求以允许所有用户使用。
+     *
+     * 该方法用于返回需要忽略的{@link RequestMatcher}的注册对象
+     * @return the {@link IgnoredRequestRegistry} to use for registering request that
+     * should be ignored
      */
-    public ChainProxyBuilder securityInterceptor(FilterSecurityInterceptor securityInterceptor){
-        this.filterSecurityInterceptor = securityInterceptor;
-        return this;
+    public IgnoredRequestRegistry ignoredRequestRegistry(){
+        return ignoredRequestRegistry;
     }
-
-    @Override
-    protected Filter performBuild() throws Exception {
-        return null;
-    }
-
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
-    }
-
-
 
     /**
      * 添加构建{@link SecurityFilterChain}的构建器实例
@@ -100,6 +152,16 @@ public final class ChainProxyBuilder extends AbstractConfiguredSecurityBuilder<F
      */
     public ChainProxyBuilder addSecurityFilterChainBuilder(SecurityBuilder<? extends SecurityFilterChain> securityFilterChainBuilder){
         this.securityFilterChainBuilders.add(securityFilterChainBuilder);
+        return this;
+    }
+
+    /**
+     * 设置{@link FilterSecurityInterceptor}。 通常由{@link com.light.security.core.config.core.configurer.WebSecurityConfigurerAdapter}调用。
+     * @param securityInterceptor
+     * @return
+     */
+    public ChainProxyBuilder securityInterceptor(FilterSecurityInterceptor securityInterceptor){
+        this.filterSecurityInterceptor = securityInterceptor;
         return this;
     }
 
@@ -113,4 +175,119 @@ public final class ChainProxyBuilder extends AbstractConfiguredSecurityBuilder<F
         this.postBuildAction = postBuildAction;
         return this;
     }
+
+    @Override
+    protected Filter performBuild() throws Exception {
+        /**
+         * 先进行{@link #securityFilterChainBuilders}的校验, 如果为空集合则停止运行
+         */
+        Assert.state(!securityFilterChainBuilders.isEmpty(),
+                "至少要给定一个SecurityBuilder<? extents SecurityFilterChain>, 通常是通过继承WebSecurityConfigurerAdapter并添加@Configuration来完成. 更高级的用户可以直接调用 "
+                        + ChainProxyBuilder.class.getSimpleName() + ".addSecurityFilterChainBuilder() 方法来注入构架对象");
+
+        // 即将进行过滤器链的创建, 可以有多个过滤器链
+        // 提前计算过滤器链集合的容量, 避免动态扩容的消耗(因为要是用ArrayList啊)
+        int chainSize = ignoredRequests.size() + securityFilterChainBuilders.size();
+        List<SecurityFilterChain> securityFilterChains = new ArrayList<>(chainSize);
+        /**
+         * 需要忽略的的请求是通过过滤器链实现的, 因为当请求到达时会先进入一个代理类, 在此时会使用已有的过滤器链集合进行匹配,
+         * 获取到匹配的过滤器链进行后续操作
+         *
+         * 这里注意:
+         * 系统中可以存在至少一条或者多条过滤器链
+         * 一条过滤器链可以零个或多个过滤器
+         *
+         * 重点又来了:
+         * 这里需要留意的是, 需要忽略的{@link ignoredRequests}中, 一则{@link RequestMatcher}
+         * 数据便对应一条过滤器链
+         *
+         * 下面便是使用需要忽略的{@link RequestMatcher}创建过滤器链
+         */
+        for (RequestMatcher matcher : ignoredRequests){
+            securityFilterChains.add(new DefaultSecurityFilterChain(matcher));
+        }
+
+        /**
+         * 系统中可以存在多条过滤器链的意义在于可以同时处理两条或多条业务线(相互之间不受影响), 比如同时作为资源服务器和认证服务器
+         *
+         * 这里是使用注入的构建器构建{@link SecurityFilterChain}, 默认情况下注入的构建对象为{@link HttpSecurityBuilder}类型,
+         * 你也可以通过实现{@link FilterChainBuilder}接口实现自己的
+         * <code> SecurityFilterChain 默认为 {@link DefaultSecurityFilterChain}</code>FilterChain构建器
+         */
+        for (SecurityBuilder<? extends SecurityFilterChain> securityFilterChainBuilder : securityFilterChainBuilders){
+            securityFilterChains.add(securityFilterChainBuilder.build());
+        }
+
+        // TODO: 2019-12-04 按顺序往下，到这里了，创建包装了List<SecurityFilterChain>的FilterChainProxy对象
+        return null;
+    }
+
+
+
+    /**
+     * An {@link IgnoredRequestRegistry} that allows optionally configuring the
+     * {@link MvcRequestMatcher#setMethod(HttpMethod)}
+     *
+     * @author Rob Winch
+     */
+    public final class MvcMatchersIgnoredRequestRegistry extends IgnoredRequestRegistry {
+        private final List<MvcRequestMatcher> mvcMatchers;
+
+        private MvcMatchersIgnoredRequestRegistry(ApplicationContext context, List<MvcRequestMatcher> mvcMatchers) {
+            super(context);
+            this.mvcMatchers = mvcMatchers;
+        }
+
+        public IgnoredRequestRegistry servletPath(String servletPath) {
+            for (MvcRequestMatcher matcher : this.mvcMatchers) {
+                matcher.setServletPath(servletPath);
+            }
+            return this;
+        }
+    }
+
+    /**
+     * Allows registering {@link RequestMatcher} instances that should be ignored by
+     * Spring Security.
+     *
+     * @author Rob Winch
+     * @since 3.2
+     */
+    public class IgnoredRequestRegistry extends AbstractRequestMatcherRegistry<IgnoredRequestRegistry> {
+
+        private IgnoredRequestRegistry(ApplicationContext context) {
+            setApplicationContext(context);
+        }
+
+        @Override
+        public MvcMatchersIgnoredRequestRegistry mvcMatchers(HttpMethod method, String... mvcPatterns) {
+            List<MvcRequestMatcher> mvcMatchers = createMvcMatchers(method, mvcPatterns);
+            ChainProxyBuilder.this.ignoredRequests.addAll(mvcMatchers);
+            return new MvcMatchersIgnoredRequestRegistry(getApplicationContext(), mvcMatchers);
+        }
+
+        @Override
+        public MvcMatchersIgnoredRequestRegistry mvcMatchers(String... mvcPatterns) {
+            return mvcMatchers(null, mvcPatterns);
+        }
+
+        @Override
+        protected IgnoredRequestRegistry chainRequestMatchers(List<RequestMatcher> requestMatchers) {
+            ChainProxyBuilder.this.ignoredRequests.addAll(requestMatchers);
+            return this;
+        }
+
+        /**
+         * Returns the {@link ChainProxyBuilder} to be returned for chaining.
+         */
+        public ChainProxyBuilder and() {
+            return ChainProxyBuilder.this;
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.ignoredRequestRegistry = new IgnoredRequestRegistry(applicationContext);
+    }
+
 }
---
+++ light-security-core/src/main/java/com/light/security/core/filter/FilterChainProxy.java
@@ -0,0 +1,227 @@
+package com.light.security.core.filter;
+
+import com.light.security.core.authentication.context.holder.SecurityContextHolder;
+import com.light.security.core.filter.chain.SecurityFilterChain;
+import com.light.security.core.util.ServletUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @ClassName FilterChainProxy
+ * @Description 核心过滤器之一 <code> FilterChainProxy </code>, 译为 过滤器链代理
+ * 该过滤器中持有{@link com.light.security.core.filter.chain.SecurityFilterChain}的集合, 即为多个<code> SecurityFilterChain </code>
+ *
+ * 该类作用: 该过滤器提供根据当前请求选择过滤器链进而执行过滤器链的功能
+ *
+ * <code> FilterChainProxy </code>的执行依赖于另一个代理类{@link org.springframework.web.filter.DelegatingFilterProxy},
+ * 这个代理类<code> DelegatingFilterProxy </code>是一个标准Servlet Filter的代理, 通过这个代理将后续执行委派给实现了过滤器接口{@link javax.servlet.Filter}的Spring Bean.
+ * 关于这个代理类的更多介绍可以参考: https://www.baeldung.com/spring-delegating-filter-proxy
+ *
+ * 小声BB:
+ * {@link org.springframework.web.filter.DelegatingFilterProxy}虽然是一个代理类, 但是也是一个名副其实的Servlet Filter Bean, 它是需要注册到ServletContainer中的,
+ * 其生命周期受Servlet容器管理. 而像Light Security中使用的Filter Bean(实现了{@link javax.servlet.Filter}接口的Spring Bean)
+ * (    例如:
+ *          {@link UsernamePasswordAuthenticationFilter}
+ *          {@link FilterChainProxy}
+ * )是没有注册到ServletContainer中的, 这里所提到的Filter Bean默认情况下是有Spring进行生命周期的管理, 由{@link org.springframework.web.filter.DelegatingFilterProxy}
+ * 代理接入标准Servlet Filter, 在该代理类的doFilter方法中委派给 "自定义的Filter Bean" 进行后续动作. 这些 "自定义的Filter Bean" 是在Spring环境下, 是可以使用Spring
+ * 带来的相关功能, 比如依赖注入.
+ * @Author ZhouJian
+ * @Date 2019-12-04
+ */
+public class FilterChainProxy extends GenericFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(FilterChainProxy.class);
+
+    private SecurityContextHolder securityContextHolder;
+
+    /**
+     * 内部维护一组过滤器链, 是后续执行的过滤器源
+     */
+    private List<SecurityFilterChain> securityFilterChains;
+
+    /**
+     * 过滤器链检查器, 默认不做检查
+     */
+    private FilterChainValidator filterChainValidator = new NullFilterChainValidator();
+
+    public FilterChainProxy(){}
+
+    public FilterChainProxy(SecurityFilterChain chain){
+        this(Arrays.asList(chain));
+    }
+
+    public FilterChainProxy(List<SecurityFilterChain> chains){
+        Assert.notEmpty(chains, "构造器接收空值参数");
+        this.securityFilterChains = chains;
+    }
+
+    @Override
+    protected void genericInit() {
+        Assert.notNull(securityContextHolder, "SecurityContextHolder 不能为null");
+        filterChainValidator.validate(this);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 暂时不做什么优化
+        try {
+            doFilterInternal(request, response, chain);
+        }finally {
+            securityContextHolder.cleanContext();
+        }
+    }
+
+    /**
+     * 核心执行流程
+     * 获取当前请求匹配的一组过滤器
+     * 根据这组过滤器创建一个虚拟的过滤器链, 注意这里的虚拟过滤器链与原始链的区别
+     * 执行虚拟过滤器链的流程, 待虚拟过滤器链执行完毕后回归原始链进行后续执行
+     * @param request
+     * @param response
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
+    private void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest internalRequest = (HttpServletRequest) request;
+        HttpServletResponse internalResponse = (HttpServletResponse) response;
+        List<Filter> filters = getFilters(internalRequest);
+
+        if (CollectionUtils.isEmpty(filters)){
+            if (logger.isDebugEnabled()){
+                logger.debug("请求: {} --> {}", ServletUtils.getServletPath(internalRequest), (filters == null ? " 没有匹配的过滤器": " 过滤器列表为空"));
+            }
+
+            chain.doFilter(internalRequest, internalResponse);
+            return;
+        }
+        /**
+         * 创建虚拟链并执行
+         */
+        VirtualFilterChain virtualFilterChain = new VirtualFilterChain(chain, filters);
+        virtualFilterChain.doFilter(internalRequest, internalResponse);
+    }
+
+    private List<Filter> getFilters(HttpServletRequest request) {
+        for (SecurityFilterChain chain : this.securityFilterChains){
+            if (chain.matches(request)){
+                return chain.getFilters();
+            }
+        }
+        return null;
+    }
+
+    public List<Filter> getFilters(String url) {
+        for (SecurityFilterChain chain : securityFilterChains){
+            /**
+             * 仿照SpringSecurity中FilterInvocation创建DummyRequest
+             */
+            if (chain.matches(new HttpServletRequestWrapper((HttpServletRequest) Proxy.newProxyInstance(HttpServletRequestWrapper.class.getClassLoader()
+                    , new Class[] { HttpServletRequest.class }
+                    , new UnsupportedOperationExceptionInvocationHandler())){
+                @Override
+                public String getServletPath() {
+                    return url;
+                }
+            })){
+                return chain.getFilters();
+            }
+        }
+        return null;
+    }
+
+    public List<SecurityFilterChain> getFilterChains(){
+        return Collections.unmodifiableList(this.securityFilterChains);
+    }
+
+    public void setFilterChainValidator(FilterChainValidator filterChainValidator){
+        Assert.notNull(filterChainValidator, "FilterChainValidator 不能为null");
+        this.filterChainValidator = filterChainValidator;
+    }
+
+    public void setSecurityContextHolder(SecurityContextHolder securityContextHolder) {
+        this.securityContextHolder = securityContextHolder;
+    }
+
+    /**
+     * 过滤器链检查器, 可以在初始化完成后进行一些检查
+     */
+    public interface FilterChainValidator{
+        void validate(FilterChainProxy filterChainProxy);
+    }
+
+    private static class NullFilterChainValidator implements FilterChainValidator{
+        @Override
+        public void validate(FilterChainProxy filterChainProxy) {
+            logger.debug("Do noting, 你可以实现 FilterChainValidator 接口完成自己的检查逻辑");
+        }
+    }
+
+    /**
+     * 核心:
+     *  内部{@code FilterChain}实现，用于通过与请求匹配的其他内部过滤器列表传递请求。
+     *
+     * 参照SpringSecurity实现
+     * 当时本想着把这个内部类提出去单独作为一个类文件, 但是想着这样破坏了原有的封装,
+     * 所以还是使用私有的内部类的方式
+     */
+    private static class VirtualFilterChain implements FilterChain {
+
+        private final FilterChain originChain;//用于记录标准的Servlet FilterChain中的FilterChain
+        private final List<Filter> additionFilters;//用于保存传入过滤器链
+        private final int size;//用于记录传入过滤器链的大小
+        private int currentPosition;//用于记录过滤器链中Filter的执行位置
+
+        private VirtualFilterChain(FilterChain originChain, List<Filter> additionFilters){
+            this.originChain = originChain;
+            this.additionFilters = additionFilters;
+            this.size = additionFilters.size();
+        }
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
+            if (currentPosition == size){
+                if (logger.isDebugEnabled()){
+                    logger.debug(ServletUtils.getServletPath((HttpServletRequest) request) + " 已经到达附加过滤器链的末端； 继续原始链");
+                }
+
+                //继续原始链
+                originChain.doFilter(request, response);
+            }else {
+                /**
+                 * 这里是先加一, 所以上面的判断条件是 <code> currentPosition == size</code>
+                 */
+                currentPosition++;
+
+                Filter nextFilter = additionFilters.get(currentPosition - 1);//所以这里要减一
+
+                if (logger.isDebugEnabled()){
+                    logger.debug("{} 已执行到附加过滤器链的第 {} 个位置, 共有 {} 个过滤器, 当前作用的过滤器为: {}", ServletUtils.getServletPath((HttpServletRequest) request)
+                            , currentPosition, size, nextFilter.getClass().getSimpleName());
+                }
+                nextFilter.doFilter(request, response, this);
+            }
+        }
+    }
+
+    private final class UnsupportedOperationExceptionInvocationHandler implements InvocationHandler {
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            throw new UnsupportedOperationException(method + " is not supported");
+        }
+    }
+}
